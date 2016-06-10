66d38a7e77084e36e95497ca9fa2bb2d4bb1ca6c
Lucas Karahadian <lucask4@gmail.com>
2016-06-10 12:27:46 -0700

    Centered everything

--- iconLayout.js
+++ iconLayout.js
@@ -5,6 +5,11 @@ function changeHeight () {
 		var style = window.getComputedStyle(icons[i]);
 		icons[i].style.height = style.width;
 	};
+	var icons = document.getElementsByClassName('icon2');
+	for (var i = icons.length - 1; i >= 0; i--) {
+		var style = window.getComputedStyle(icons[i]);
+		icons[i].style.height = parseInt(style.width) * .79;
+	};
 }
 
 changeHeight();
