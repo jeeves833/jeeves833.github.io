fa184b41ef4b5afe4f5d8327f25c07ffea04d5a5
Lucas Karahadian <lucask4@gmail.com>
2016-06-15 16:56:32 -0700

    Icons now fully dynamically change size with the page.

--- iconLayout.js
+++ iconLayout.js
@@ -1,15 +1,71 @@
 function changeHeight () {
-	console.log('changing')
 	var icons = document.getElementsByClassName('icon1');
 	for (var i = icons.length - 1; i >= 0; i--) {
 		var style = window.getComputedStyle(icons[i]);
 		icons[i].style.height = style.width;
+		var heading = icons[i].getElementsByTagName('h2')[0];
+		heading.style.fontSize = parseFloat(style.width) * .044;
+
 	};
 	var icons = document.getElementsByClassName('icon2');
 	for (var i = icons.length - 1; i >= 0; i--) {
 		var style = window.getComputedStyle(icons[i]);
 		icons[i].style.height = parseInt(style.width) * .79;
+		var heading = icons[i].getElementsByTagName('h2')[0];
+		heading.style.fontSize = parseFloat(style.width) * .056;
 	};
 }
 
-changeHeight();
+function setThreeColMargins () {
+	var fullWidth = parseInt(window.getComputedStyle(document.body).width);
+	var sections = document.getElementsByClassName('three-column');
+	for (var i = sections.length - 1; i >= 0; i--) {
+		var icons = sections[i].getElementsByClassName('icon');
+		if (icons.length < 1) {
+			continue;
+		};
+		var marginSize = (fullWidth - 3 * (parseFloat(window.getComputedStyle(icons[0]).width) + 2))/4;
+		for (var i = icons.length - 1; i >= 0; i--) {
+			icons[i].style.marginLeft = marginSize.toString() + "px";
+		};
+	};
+}
+
+function setOneColMargins () {
+	var fullWidth = parseInt(window.getComputedStyle(document.body).width);
+	var sections = document.getElementsByClassName('one-column');
+	for (var i = sections.length - 1; i >= 0; i--) {
+		var icons = sections[i].getElementsByClassName('icon');
+		if (icons.length < 1) {
+			continue;
+		};
+		var marginSize = (fullWidth - (parseFloat(window.getComputedStyle(icons[0]).width) + 2))/2;
+		for (var i = icons.length - 1; i >= 0; i--) {
+			icons[i].style.marginLeft = marginSize.toString() + "px";
+		};
+	};
+}
+
+function setTwoColMargins () {
+	var fullWidth = parseInt(window.getComputedStyle(document.body).width);
+	var sections = document.getElementsByClassName('two-column');
+	for (var i = sections.length - 1; i >= 0; i--) {
+		var icons = sections[i].getElementsByClassName('icon');
+		if (icons.length < 1) {
+			continue;
+		};
+		var marginSize = (fullWidth - 2 * (parseFloat(window.getComputedStyle(icons[0]).width) + 2))/3;
+		for (var i = icons.length - 1; i >= 0; i--) {
+			icons[i].style.marginLeft = marginSize.toString() + "px";
+		};
+	};
+}
+
+function setLayout () {
+	changeHeight();
+	setThreeColMargins();
+	setOneColMargins();
+	setTwoColMargins();
+}
+
+setLayout();
