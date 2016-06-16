26880205b4d0724d3631e39bd4f1f3aa39ca7a11
Lucas Karahadian <lucask4@gmail.com>
2016-06-16 16:47:13 -0700

    Made margin changes less abrupt

--- iconLayout.js
+++ iconLayout.js
@@ -1,63 +1,29 @@
 function changeHeight () {
 	$(".icon").each(function() {
 		var icon = $(this);
-		console.log(icon.width());
 		icon.width(Math.floor(icon.data("width") * $("body").width()));
 		icon.height(Math.floor(icon.width() * icon.data("aspect")));
-		console.log(icon.children("h2").css("fontSize", icon.width() * icon.data("fontAspect")));
+		icon.children("h2").css("fontSize", icon.width() * icon.data("fontAspect"))
 	});
-}
+};
 
-function setThreeColMargins () {
-	var fullWidth = parseInt(window.getComputedStyle(document.body).width);
-	var sections = document.getElementsByClassName('three-column');
-	for (var i = sections.length - 1; i >= 0; i--) {
-		var icons = sections[i].getElementsByClassName('icon');
+function setColMargins () {
+	var fullWidth = $('body').width();
+	$("[class|='columns']").each(function() {
+		var section = $(this);
+		var numColumns = parseInt(section.attr("class").slice(-1));
+		var icons = section.find('.icon');
 		if (icons.length < 1) {
-			continue;
-		};
-		var marginSize = Math.floor((fullWidth - 3 * (parseFloat(window.getComputedStyle(icons[0]).width) + 2))/4);
-		for (var i = icons.length - 1; i >= 0; i--) {
-			icons[i].style.marginLeft = marginSize.toString() + "px";
-		};
-	};
-}
-
-function setOneColMargins () {
-	var fullWidth = parseInt(window.getComputedStyle(document.body).width);
-	var sections = document.getElementsByClassName('one-column');
-	for (var i = sections.length - 1; i >= 0; i--) {
-		var icons = sections[i].getElementsByClassName('icon');
-		if (icons.length < 1) {
-			continue;
-		};
-		var marginSize = (fullWidth - (parseFloat(window.getComputedStyle(icons[0]).width) + 2))/2;
-		for (var i = icons.length - 1; i >= 0; i--) {
-			icons[i].style.marginLeft = marginSize.toString() + "px";
-		};
-	};
-}
-
-function setTwoColMargins () {
-	var fullWidth = parseInt(window.getComputedStyle(document.body).width);
-	var sections = document.getElementsByClassName('two-column');
-	for (var i = sections.length - 1; i >= 0; i--) {
-		var icons = sections[i].getElementsByClassName('icon');
-		if (icons.length < 1) {
-			continue;
-		};
-		var marginSize = (fullWidth - 2 * (parseFloat(window.getComputedStyle(icons[0]).width) + 2))/3;
-		for (var i = icons.length - 1; i >= 0; i--) {
-			icons[i].style.marginLeft = marginSize.toString() + "px";
-		};
-	};
+			return;
+		}
+		var marginSize = Math.floor((fullWidth - numColumns * (icons.width() + 2))/(numColumns + 1));
+		icons.css('marginLeft', marginSize);
+	});
 }
 
 function setLayout () {
 	changeHeight();
-	setThreeColMargins();
-	setOneColMargins();
-	setTwoColMargins();
+	setColMargins();
 }
 
 window.onresize = function () {
